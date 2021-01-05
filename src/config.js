63131ef7fdfeb0a89494a72b75142a88c86f42ca
Joel Del Cueto <wayfaringjou@gmail.com>
2021-01-05 18:06:13 -0500

    Add setup for deployment

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DB_URL: process.env.DB_URL || 'postgresql://postgres:password@localhost:5432/noteful-api',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/noteful-api',
 };
